a67f874c1109b053bc079ee93717b1cf324c056f
Johan Lundström <johanlundstrom@c-83-233-144-8.cust.bredband2.com>
2009-08-14 19:23:24 +0200

    Add list_contents and parse_basic_header. Nonworking! Coding at case-statement for ':marker'

--- rarfile.rb
+++ rarfile.rb
@@ -1,24 +1,17 @@
 class RarFile
-  
-  MARKER_BLOCK = 0x72
-  ARCHIVE_BLOCK = 0x73
-  FILE_BLOCK = 0x74
-  
-  STORE_METHOD = 0x30 # no compression or encryption
-  
+  BLOCK_TYPES = [:marker, :archive, :file]
   OSES = [:msdos, :os2, :win32, :unix, :mac, :beos]
   
   BASIC_FIELDS = [:crc, :type, :flags, :header_size]
   FILE_FIELDS = [
-    :packed_size, :unpacked_size, :os, :file_crc, :file_time, :rar_version, 
-    :pack_method, :name_length, :file_attrs, :extra_packed_size, :extra_unpacked_size
+    :unpacked_size, :os, :file_crc, :file_time, :rar_version, :pack_method,
+    :name_length, :file_attrs
   ]
   
   def initialize(filename)
     @fh = File.open(filename, 'rb')
-    p parse_header(true) # marker block
-    p parse_header(true) # archive block
-    p parse_header(true) # first file
+    @files = nil
+    p parse_header(true) # skip marker block
     
     if block_given?
       yield self
@@ -30,43 +23,73 @@ class RarFile
     new(*args, &block)
   end
   
+  def list_contents
+    @fh.seek(7, IO::SEEK_SET)
+    p parse_header(true) # skip archive block
+    @files = []
+      
+    begin
+      loop do
+        block = parse_header(true)
+        @fh.seek(block[:data_size], IO::SEEK_CUR) if block[:data_size] # skip file data
+        # @files << block if block[:type] == :file
+        p block
+      end
+    rescue EOFError
+    end
+    
+    @files
+  end
+  
   private
   
     def parse_header(want_return = false)
       block = {}
-      block[:block_start] = @fh.tell
-      basic_header = assign_block_fields(@fh.read(7).unpack("vCvv"), block, BASIC_FIELDS)
+      parse_basic_header(block)
       
       case block[:type]
-      when MARKER_BLOCK
-        raise 'not a rar file' if basic_header != [24914, 114, 6689, 7]
-      when ARCHIVE_BLOCK
+      when :marker
+        p BASIC_FIELDS.map { |v| block[v] }
+        raise 'not a rar file' if BASIC_FIELDS.map { |v| block[v] } != [24914, 114, 6689, 7]
+      when :archive
         # do nothing
-      when FILE_BLOCK
+      when :file
         want_return = true
         parse_file_header(block)
       else
-        raise 'unsupported block type'
+        raise 'unsupported block type' unless block[:skip_if_unknown]
       end
       
-      block[:header_ending] = block[:block_start] + block[:header_size]
+      block[:header_ending] = block[:header_start] + block[:header_size]
       @fh.seek(block[:header_ending], IO::SEEK_SET)
       
       block if want_return
     end
     
+    def parse_basic_header(block)
+      block[:header_start] = @fh.tell
+      assign_block_fields(@fh.read(7).unpack("vCvv"), block, BASIC_FIELDS)
+      
+      if block[:flags] & 0x8000 != 0
+        block[:data_size] = @fh.read(4).unpack("V")[0]
+      end
+      
+      block[:type] = BLOCK_TYPES[block[:type] - 0x72]
+      block[:skip_if_unknown] = block[:flags] & 0x4000 != 0
+    end
+    
     def parse_file_header(block)
-      assign_block_fields(@fh.read(33).unpack("VVCVVCCvVVV"), block, FILE_FIELDS)
+      assign_block_fields(@fh.read(21).unpack("VCVVCCvV"), block, FILE_FIELDS)
       
       if block[:flags] & 0x100 != 0
-        block[:packed_size] += block[:extra_packed_size] << 32
-        block[:unpacked_size] += block[:extra_unpacked_size] << 32
-      else
-        @fh.seek(-8, IO::SEEK_CUR)
+        extra_data_size, extra_unpacked_size = @fh.read(8).unpack("VV")
+        block[:data_size] += extra_data_size << 32
+        block[:unpacked_size] += extra_unpacked_size << 32
       end
       
       block[:file_name] = @fh.read(block[:name_length]).unpack("a*")[0]
-      raise 'unsupported pack method' if block[:pack_method] != STORE_METHOD
+      block[:pack_method] -= 0x30 # becomes 1..5
+      raise 'unsupported pack method' if block[:pack_method] != 0 # no encrypytion/compression
       block[:os] = OSES[block[:os]]
     end
     
@@ -77,6 +100,6 @@ end
 
 if $0 == __FILE__
   RarFile.open(ARGV.pop) do |f|
-    puts f.inspect
+    puts f.list_contents
   end
 end
